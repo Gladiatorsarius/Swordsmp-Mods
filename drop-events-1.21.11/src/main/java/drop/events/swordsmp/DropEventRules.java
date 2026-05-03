1a9599c5b49378f51f07272db62d17a90eefcf42
Gladiatorsarius <pmitze@gmx.de>
2026-05-03 14:56:48 +0200

    Added GameRule Schulker_Health

--- drop-events-1.21.11/src/main/java/drop/events/swordsmp/DropEventRules.java
+++ drop-events-1.21.11/src/main/java/drop/events/swordsmp/DropEventRules.java
@@ -9,16 +9,29 @@ import net.minecraft.world.level.gamerules.GameRuleCategory;
  * Holds Drop Events custom gamerules.
  */
 public final class DropEventRules {
+
 	public static GameRule<Boolean> GUARD_VISIBLE;
+	public static GameRule<Integer> SHULKER_HEALTH;
 
 	private DropEventRules() {
 	}
 
 	public static void register() {
+
 		if (GUARD_VISIBLE == null) {
 			GUARD_VISIBLE = GameRuleBuilder.forBoolean(false)
 				.category(GameRuleCategory.MOBS)
-				.buildAndRegister(Identifier.fromNamespaceAndPath(DropEvents.MOD_ID, "visible_guard"));
+				.buildAndRegister(
+					Identifier.fromNamespaceAndPath(DropEvents.MOD_ID, "visible_guard")
+				);
+		}
+
+		if (SHULKER_HEALTH == null) {
+			SHULKER_HEALTH = GameRuleBuilder.forInt(1000) // default = 1000
+				.category(GameRuleCategory.MOBS)
+				.buildAndRegister(
+					Identifier.fromNamespaceAndPath(DropEvents.MOD_ID, "shulker_health")
+				);
 		}
 	}
 }
